6fa15e66d58028f8c9a79f43331787b678f50945
Makarenkov Michael <m--m@i.ua>
2016-02-04 11:29:28 +0200

    Simple backlinks checker PHP
    Show good or bad backlinks, for personal using

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+//name="url"
+//comment-form-url
+//по curl опредеяем есть ли строка на странице, выводим отчет
+// сохраняем в память базовый домен
+
+include 'simple_html_dom.php';
+
+
+$backlinks = file("backlinks.csv");
+$h = fopen("res.csv", "w");
+
+foreach ($backlinks as $link) {
+    $ok = 'bad';
+    $link = str_replace("\n", "", $link);
+    $link = str_replace("\r", "", $link);
+    $html = file_get_html($link);
+    $parse = parse_url($link);
+    if(isset($host[$parse['host']])) {
+        continue;
+    }
+    @$host[$parse['host']]=1;
+    
+    if ($html) {
+        
+        if (strpos($html, 'comment-form-url') !== FALSE) {
+            $ok = 'good';
+        }
+
+        if (strpos($html, 'name="url"') !== FALSE) {
+            $ok = 'good';
+        }
+        
+        if (strpos($html, 'nofollow') !== FALSE) {
+            $ok = 'nofollow';
+        }
+        
+       
+    }
+    if($ok == 'good' OR $ok == 'nofollow') {
+		fwrite($h, $link . ";" . $ok . "\n");
+    }
+}
+
+
